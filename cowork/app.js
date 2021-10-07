f47ed5282eddc19ffb2ba09a5dad33fe80b17e02
JinAru16 <70217158+JinAru16@users.noreply.github.com>
2021-10-07 14:19:41 +0900

    delete를 이용하여 회원정보를 삭제하는 메서드를 만듦

--- cowork/app.js
+++ cowork/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express(); // express로 만든 객체는 app이라고 관습적으로 이름을 붙임
 
-const members = require('./members');
+let members = require('./members');
 
 app.use(express.json()); // request가 rout handller에게 처리되기 전에 필요한 전처리를 하는 함수를 express에서는 미들웨어라고 함
 
@@ -16,6 +16,9 @@ app.get('/api/members', (req, res) =>{
     }
 });
 
+
+
+// 회원 정보를 찾는 메소드
 app.get('/api/members/:id', (req, res) =>{
     const { id } = req.params;
     const member = members.find((m) => m.id === Number(id));
@@ -25,23 +28,69 @@ app.get('/api/members/:id', (req, res) =>{
     members에 있는 id랑 같으면 True를 리턴하게 함.
     */
 
-    
     if (member) {
         res.send(member);
+        /*
+        그리고 아니 그럼 res.send(member) 하면 트루를 리턴하라는거잖아 가 아니고
+        그 트루로 나타난 객체를 내보내란 소리임.
+        */
+
     } else {
-        res.status(404).send({message:'There is no such member'});
+        res.status(404).send({message:'There is no member with the id!!'});
     }
 });
 
 app.post('/api/members', (req, res) => {
     const newMember = req.body;
     members.push(newMember);
-    re.send(newMember);
+    res.send(newMember);
 });
 
+
+// 회원 정보를 수정하는 put 리퀘스트
+app.put('/api/members/:id', (req, res) => {
+    const {id} = req.params;
+    const newInfo = req.body;
+    const member = members.find((m) => m.id === Number(id));
+    if (member) {
+        // object 객체에 keys를 사용하면 특정 객체의 모든 프로퍼티를 조회가능
+        Object.keys(newInfo).forEach((prop) => {
+            member[prop] = newInfo[prop];
+        });
+        res.send(member);
+    } else {
+        res.status(404).send({ message : 'There is no member with the Id'})
+    }
+});
+
+
+// 회원 정보를 삭제하는 delete 리퀘스트
+app.delete('/api/members/:id', (req, res) => {
+    const { id } = req.params;
+    const membersCount = members.length;
+    members = members.filter((member) => member.id !== Number(id));
+    /*
+    입력한 id 이외의 id들을 갖고있는 객체들만 추린다는 뜻. 
+    */
+    if (members.length < membersCount) {
+        res.send({ message: 'Deleted'});
+    } else {
+        res.status(404).send({ message: 'There is no member with the id!'});
+    }
+});
 
 
 app.listen(3000, () => {
     console.log('Server is listening...');
     
 }); 
+
+
+/*
+package.json파일에 script:{
+    "start": node app.js
+} 를 등록하면 터미널에 npm start로 간단하게 서버 실행 가능
+
+그리고 npm에서 직접 지정한 명령어 말고 내가 임의로 지정한 명령어들
+가령 dev같은건 앞에 run을 붙여줘야함. ex : npm run dev이렇게 
+*/
