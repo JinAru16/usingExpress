8dac6de36b38eda369ce3b190d097895e90df4e4
JinAru16 <70217158+JinAru16@users.noreply.github.com>
2021-10-19 00:44:16 +0900

    모델과 테이블 연동하기

--- cowork/app.js
+++ cowork/app.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const app = express(); // express로 만든 객체는 app이라고 관습적으로 이름을 붙임
 
-let members = require('./members');
+const db = require('./models/');
+
+const { Member } = db;
 
 app.use(express.json()); // request가 rout handller에게 처리되기 전에 필요한 전처리를 하는 함수를 express에서는 미들웨어라고 함
 
